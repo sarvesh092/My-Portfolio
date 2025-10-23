d9530273ce3d79873a575aa96a5b2c3ab0132710
sarvesh <kashyapsarvesh092@gmail.com>
2025-10-23 21:32:55 +0530

    initalize project and added required icons

---
+++ src/custom.d.ts
@@ -0,0 +1,11 @@
+declare module '*.svg?react' {
+  import { FC, SVGProps } from 'react';
+  const ReactComponent: FC<SVGProps<SVGSVGElement>>;
+  export { ReactComponent };
+  export default ReactComponent;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import path from "path";
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
